fab8184d6c97fadbed54ae5ca84c7e38ca1c5a17
MD. YOUSUF ALI APEL <yousufailapel785@gmail.com>
2026-08-18 20:03:56 +0600

    Add Department and Appointment views

---
+++ HMS/Views/Appointments/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HMS.Views.Appointments
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
