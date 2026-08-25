6bbc4fea972a1b44318002b23fde73a7df3fd5d5
Ananna2000 <ananna.anu401@gmail.com>
2026-08-25 09:21:02 +0600

    Add Patient model and views

---
+++ HMS/Models/Patient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalManagement.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int PatientId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Patient Name")]
+        public string PatientName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Phone]
+        [StringLength(20)]
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [StringLength(250)]
+        [Display(Name = "Address")]
+        public string? Address { get; set; }
+    }
+}
